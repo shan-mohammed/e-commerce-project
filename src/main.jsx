0bc0bdf613f744283550669fbd2d5d26eaf4a343
shan-mohammed <shanmhd380@gmail.com>
2026-08-07 09:25:40 +0530

    added redux , appied store and cart slce and modified some files

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import{router} from "./routes/Router.jsx"
-
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
+
    <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
---
+++ src/redux/cartSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+
+      const existingItem = state.items.find(
+        (item) => item.id === product.id
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...product,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+
+    increaseQty: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item) item.quantity++;
+    },
+
+    decreaseQty: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item && item.quantity > 1) {
+        item.quantity--;
+      }
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  increaseQty,
+  decreaseQty,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
